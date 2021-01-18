63d862def697c5154e82f8f39af3fd3a53902da5
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2021-01-18 09:47:55 +0100

    add storybook

---
+++ .storybook/theme.js
@@ -0,0 +1,9 @@
+
+import { create } from '@storybook/theming/create';
+export default create({
+  base: 'light',
+  brandTitle: 'Controls',
+  brandUrl: 'https://github.com/carlosthe19916/controls-ui',
+  brandImage: 'https://raw.githubusercontent.com/carlosthe19916/controls-ui/master/public/logo192.png',
+});
+
