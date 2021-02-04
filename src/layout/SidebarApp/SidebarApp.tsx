a9f8401c6996f93eecf80a3166f474bef91576f1
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2021-02-04 17:58:18 +0100

    Add provisional logos

--- src/layout/SidebarApp/SidebarApp.tsx
+++ src/layout/SidebarApp/SidebarApp.tsx
@@ -6,6 +6,9 @@ import { Nav, NavItem, PageSidebar, NavList } from "@patternfly/react-core";
 import { Paths } from "Paths";
 import { LayoutTheme } from "../LayoutUtils";
 
+import styles from "./SidebarApp.module.scss";
+import redHatLogo from "images/red-hat-horizontal-reverse.svg";
+
 export const SidebarApp: React.FC = () => {
   const { t } = useTranslation();
 
@@ -32,6 +35,9 @@ export const SidebarApp: React.FC = () => {
             </NavLink>
           </NavItem>
         </NavList>
+        <section className={styles.rhLogoSection}>
+          <img src={redHatLogo} alt="Red Hat" />
+        </section>
       </Nav>
     );
   };
