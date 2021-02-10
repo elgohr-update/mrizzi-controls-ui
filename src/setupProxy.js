5934a23a16f3f28b58ee9d1f7fea4503eba480e9
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2021-02-10 15:22:13 +0100

    remove upshift urls

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,8 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api/controls",
     createProxyMiddleware({
-      target: "http://controls-mta-pathfinder.apps.ocp4.prod.psi.redhat.com",
-      // target: "http://localhost:8080",
+      target: "http://localhost:8080",
       changeOrigin: true,
       pathRewrite: {
         "^/api/controls": "/controls",
